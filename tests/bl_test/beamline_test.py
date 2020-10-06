84077e90ddfbf48c4a9a893b838d2eb8e00ea984
Trey <twguest@students.latrobe.edu.au>
2020-10-06 15:05:49 +1100

    Final Update Pre Namechange

--- tests/bl_test/beamline_test.py
+++ tests/bl_test/beamline_test.py
@@ -12,9 +12,7 @@ A scratchpad for testing beamline methods
 
 ###############################################################################
 import sys
-sys.path.append("/opt/WPG/") # LOCAL PATH
-sys.path.append("/gpfs/exfel/data/user/guestt/WPG") # DESY MAXWELL PATH
-
+ 
 sys.path.append("/opt/spb_model") # LOCAL PATH
 sys.path.append("/gpfs/exfel/data/user/guestt/spb_model") # DESY MAXWELL PATH
 ###############################################################################
