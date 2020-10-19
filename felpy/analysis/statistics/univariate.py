fb5ebf2d122aca96af458c1ce9ff322b2e91cc8c
Trey <twguest@students.latrobe.edu.au>
2020-10-19 15:39:39 +1100

    Update to Univariate Stats

--- felpy/analysis/statistics/univariate.py
+++ felpy/analysis/statistics/univariate.py
@@ -10,5 +10,9 @@ def mean_intensity(ii, mode = 'train'):
     if mode == 'all':
         mi = np.mean(ii, axis = -1).mean(axis = -1)
     if mode == 'train':
-        mi = np.mean(ii, axis = 2)
+        mi = np.mean(ii, axis = -1)
+    if mode == 'pulse':
+        mi = np.mean(ii, axis = -2)
+    if mode == 'shape':
+        mi = ii.mean(axis = 0).mean(axis = 0).mean(axis = -1)
     return mi
