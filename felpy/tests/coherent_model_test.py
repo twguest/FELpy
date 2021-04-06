9b5d374511bb645496d9ce58cc3fe556c0b388aa
Trey Guest <twguest@students.latrobe.edu.au>
2021-04-06 11:12:14 +1000

    Auto stash before merge of "maxwell" and "origin/maxwell"

--- felpy/tests/coherent_model_test.py
+++ felpy/tests/coherent_model_test.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from felpy.model.beamlines.exfel_spb.methods import get_beamline_object
-from felpy.model.src.coherent import construct_SA1_wavefront
-from wpg.wpg_uti_wf import plot_intensity_map
-from felpy.model.tools import propagation_parameters
+from felpy.model.beamlines.methods import get_beamline_object
+from felpy.model.src.coherent import construct_SA1_pulse
+
 
 if __name__ == '__main__':
-    wfr = construct_SA1_wavefront(1024, 1024, 9.0, 0.25)
+    wfr = construct_SA1_pulse(1024, 1024, 5, 5.0, 0.25)
     spb = get_beamline_object(apertures = True, surface = "on")
-    spb.propagation_options[0]['optical_elements'][-1].L += 2.2
-
-    spb.propagation_options[0]['propagation_parameters'][-1] = propagation_parameters(2,1,2,1,mode = 'quadratic')
-      
     spb.propagate_sequential(wfr)
- 
-    plot_intensity_map(wfr)
